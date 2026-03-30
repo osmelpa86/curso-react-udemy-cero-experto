eebd30b9c92e58a7d3520cca431c108bcfcccf60
osmelpa86 <osmelpa86@gmail.com>
2026-03-29 22:26:28 -0600

    memorama

---
+++ 7-memorama/src/components/Tablero/Tablero.jsx
@@ -0,0 +1,18 @@
+import { MemoBlock } from "../MemoBlock/MemoBlock";
+import "./Tablero.css";
+export function Tablero({ animating, handleMemoClick, memoBlocks }) {
+  return (
+    <main className="board">
+      {memoBlocks.map((item, index) => {
+        return (
+          <MemoBlock
+            key={`${index}_${item.emoji}`}
+            memoBlock={item}
+            animating={animating}
+            handleMemoClick={handleMemoClick}
+          />
+        );
+      })}
+    </main>
+  );
+}
